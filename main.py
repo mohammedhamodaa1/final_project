a59eeea0610187c84a601007ff4621d06d6b3093
mohammedhamodaa1 <mohammedhamodaa1@gmail.com>
2023-08-29 23:06:11 +0300

    Todo 4

--- main.py
+++ main.py
@@ -2,14 +2,22 @@
 Name ="mohammed Emad Hamoda"
 Delivery_Date = "29/8/2023"
 
+
 import uuid
-
-
 class Course:
     def __init__(self, course_name, course_mark):
         self.course_id = str(uuid.uuid4())
         self.course_name = course_name
         self.course_mark = course_mark
 
+
 class Student:
     total_student_count = 0
+
+    def __init__(self, student_name, student_age, student_number):
+        self.student_id = str(uuid.uuid4())
+        self.student_name = student_name
+        self.student_age = student_age
+        self.student_number = student_number
+        self.courses_list = []
+        Student.total_student_count += 1
